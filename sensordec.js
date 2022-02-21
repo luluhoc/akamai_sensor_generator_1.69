4b5e4e370bf95390f5193e77ce2736036708c4cf
luluhoc <lucjan.grzesik@gmail.com>
2022-02-21 16:44:41 +0000

    c

---
+++ sensordec.js
@@ -0,0 +1,77 @@
+const d = "{\"sensor_data\":\"2;3291446;4338228;4,17,0,0,1;w}c?d{fR_r>H)Fo0dh}ZpL*}jis%&JdEz$ WH9-9u~elg`q{VY}f ~t$ZuFrbjY^<ySdyfq-ou6d.^D^9Rqcg+c~)EmP(Xv?JW==o@<(G6s>/aE,0YIIPu@gl?<b4hODYhTgZ:-.K_9Rzn1L%LZ6he7KDZo6b>`:=YWbBt{,JwP)#]C0b+V_sM|~4XjHdGq>N#+/o@Vd;RP~w^`;uHy/?~XggDS=_CtSqCD_9#0=!wnQwRM1)T0APC)CF|:zU<_Lq;MsW9TjE1MFTFI93Y4ctaBelKvV/{WFnIRdeNj c==;A02zy_EU3@Jy_Ywmrc,&U=Gj^W~B&_iUlbnd[H)lNf]5D(_I84k_@!BbYMsK3 #s&%;21{w9nd_+N>hjMJF+lVlz !C2GTaj$7RqA2$XyRd3A:kvak`N5!OjJV1fkP21WVlv9s!Tmj HKkZZ6vON9u$KhB VNq~5XZ*.*}~F^:C/hPri]sv~gQ.a},wOi)dwW4s#PFN+x%@rMQ<MRp^udeK$8R;+~!I`!B!$IZ]G;BZB;bp14ISoYmA5Re^=BU#2`&bwBOhC2Lv`BKAfs4DH!6q-?ulJkn?`<S9W.7HNbLtiHhb+7]c64;3uYJPV4bOrb*a/_w: 3O|nSrs:Ot3Wjua`6ZjV{!j*o97S-@og`.%EIj)Si6R&*F@HvCRFr&h7(bGuN%w3<tHOKkyIbBe9WlnoCo.1Z!b6}aI}u+Z^cI%DhJb@fFqAS(20hRQbCU@zobB3t>v 9vO^e4F,OCm6[-0Gy`w7d6#qD!~dH->m~,g~F{;f4(E7[0BE:y4P#.5<>*4vf<t3J?*@F IDbZ;{Nw2B>0?bE*$`xZeQV8u#Q@2Oh>x[C8jU}ewB6%OcR1-/4-13*oHr<=&XpG~tWdlyU&a*{u=n^EK=IISUS72O|~uMfU.(f[WG2r&[af&OL*D:`ajtnqd+/Zg/h|% qt78qdf-,2b?f&4@XXzx.,NY41JGA<xAgr4sPsKIO9:yr<$0YV%%t5:oN_cyArUcRnWjRT+fh#eGR9aa8Cp%<.=sj~.r4K39>UDXRX,]}:%gqg):g-W@kfC{l&8*YfbiGsu)(P!ih.qojS,6&,wGPW-tFYS.o l0Ehhx}{m_zB&5e3c)b2m OYy^TS{jKp,N[{,HTdJ?d%#v2A4xlG&L.9HM9m77~-@am:Asz8{uDxne6Gv=T2e7$[wwqK(~j}2SpIb5FMQN)HFEw6s6/^PrTvAM?uDPI7z)jK>>IB;hB ~]Mtba,ZQQ:&&^9o +Wtg;a9Z:QCv|_6Dz*E>q^tA&20dJx]kGOx5kB~G#Dws(mN_oNjdC@}0m[1_x%kXpe;bCpZ,r-:Gdo]n![2,2DO[E<hVg@ESr~X!mE+bJwMT{bPo3qLN8>z0zwZ2-Z5[o*l7~UfbcC{A`NFk,vHU?NSPPK1 5[GRu|`<*lfDDnYr=6)TnrDWDYWc_eX8^=3*Tq_5%|oTV)x5cpb1cN5?:cewh-db5gl[8m<(*nrYbF0]#(,_/9%>?NVuv1#vC*27g)-hx_ciXA2]5FQ5fa`rk7WCa-! d9C]2eY@E6MB[G;jBbYP#-}JGx+Wiwz|ryK_K!wnx|3,<0#jHWuT>@=dy@`-CopigrDdjd.-CGZXn^mJ<DtU:KW#5w}dmDBQ6+RgU1@8a^$)G>N0v@iMLGk-M1K+8ny2|p6^U6AFexA>}:jXRsZw&<qfB/Z0^2Kf][{E3}>9`nMOy(7$ Gxyn0D S,cJcBa %~W|vv 7UoJa,A[KCxREFp$}quTKtXgD>2n?EDe!;[.L/6b^P5qow=hWwhI<q[|AJO=njr1@$EzAz;f^fD}29i+zN>0$UTlNKZG($%ZuPsT$`%PUkE-3I,P>$|Zm<.Q0@_>{>K~9kK-T8a3JK; @Q*^R8G0$lk:y*I7.8LxE-hJ s8pt;0{BB5olZMKJF=pUr.${:QyF?ld=7c=]fde{I|^f^dMp`U4-AinQv*pO: mVH#euA2)_)p_V]WJ<9N(7j=34_ d0NHhWS,w:c*>VH_0?9Tbjs bT$QdZ2T;v{k^UW/(>rp!Z%#n*,LQp`|n].iy{TqoMaJYGE,~O&,33w26j0m~SPiW57z{(oA5|%}/6bns<v+H*baan!Rk25QPN/t3@|NIBQIX^`SS8g.5&lri2Fp%]k85D!m):$[]Sbgz#,0@te{?0[myS`0Y*6ko~iIe++SvZx 6QhF^K$v,{t?9M(FDCslL453o}sb<WA;]-&`-Bsi%y7B~U.S1bA_j@u#?ok@vtHE2SDXiU,d^sox?B%@g#j^ NZv~3%lOCRD:C8GZy)w]:WGX{2:Oe<f}il+aSr2;Lj{Cy=S`Es>jw3zrW^ZBdy<b#R<!^ap/D!:./iQrk)4xo#h^tjSvuKd[BZu(#17%%G9m1AZaT3V#?)u-O=H2+Q<&ui=Dh1qr+U:dMIY5uHxF83p7d1o(NfYn?*9eT1}T^8vK:q}%Ha{G*L}U9>usneHT(E$Yocbi@(F]z@Mo(%X;k%~+iv>jjA|a(!Yi:@D__aX2|E98Po}jX$!rP* sjHvm%{Z:|Tsn0.IwrdY:Isgwsuw]^_:3Bq8i~*E(EXd)<fF0Iu^lQw}yfqf*7$Gka7|*)FmPH^x/Bx;8?W{]K@?Of1_oIFnZ^u`QkP5@)^Jq_l=bXjcpMoeDQ=+[L:6Y=n%e,)dsgW0-KEA5VoR:$Q2A!5lp$yO/Oqd`%<)CB WFwud>Z[R6Z!p>(b$(Cm=V9Mlo&rdl{W0mf~ ILihhT;O/?LnuJ]8oj(amH6*KcTMOS~yKL%PxgqVB9-J]4QB/1O1C_YJv?)$(9Wo?Hlp-*vI{lf|6t5wJ/M.;;B3l_U=kp8Yx.lq;yeJz-}@jBsb){9}HnqeTHjc{5^ X7*JtsyK161_}h?&SV(i-R]eI{<N1X7Rk4)N-Zm??l4k1pQ@}{O>gGJU@x%2TtGwN|buI?]4t-SH&e<Du9}s?hwfYIPh:]A9//x>l|8[&Nan>4eYdd1+r):V2x2?aC?&zI2Q9FGBHh,n?vbwd]IR)i/<+:zJ.cO!#CFyI^$kd}D5hpPjWcaN1|=br1c&xD2sgK6vHt/0{JtV;GAEL_OWOh=s!|?`B~lLK3)`Vv:0;d2.mqrz5F<H4.O# p&MAC5)&ywZ:e9Z|j<=/:DZZ(~1~kO&&$tjmWhY]NX7 Q1654!<8Ud{/LMgee,$W~d:/#(t-x$fk0u#:uKQBgk6En~<8Bvap h3! %*9<E57_$FX[36:^(Erf*TUrO6AXJv)w58HLXWQLS#P,72F^tCi7Fmw/k`v;xhn`|v;KmWsQ:Wro_jwF`Ba9]|#N-.VlV>OMp{4?rmlVNlA,*9j^.#K#B{*H1N3vtkx(SR%0]ku__|NJ?c}R|X5x@l./M>s@=#LOT43x:{A]S)4Yb tLr-fKM&BE><ktyEXp?_/SQ3T#!M2`a*J6aY.zQ2Z|&3dIEJbuXvTkK[ $5sL]X&]E}[?=jZ-SeqL6@?l)^!g1n}[]lK&<;_%MrmW]6&ph(E1s9Jo=x(oMBUq&/vn~/gE2v\"}"
+
+
+const extract = d.substring("{\"sensor_data\":\"".length, d.length - 2)
+
+// console.log(extract);
+const match = extract.match(/^([\d]+\;[\d]+\;[\d]+\;[\d]+\,[\d]+\,[\d]+\,[\d]+\,[\d]+;)/s);
+function Bc(b8, m8) {
+  return b8 % m8;
+}
+
+function Gf(sf, gF) {
+  return sf & gF;
+}
+
+function Lc(mc, zc) {
+  return mc >> zc;
+}
+
+function Wq(xq, nq) {
+  var b6 = "";
+  var m6 = "";
+  var j6 = new Array(16);
+
+  if (m6.length === 0) {
+    for (var i = 0; i < 127; i++) {
+      if (i < 32 || i == 39 || i == 34 || i == 92) {
+        console.log('test')
+        arraySet('set', i, j6, -1);
+      } else {
+        arraySet('set', i, j6, m6.length);
+        m6 += String.fromCharCode(i).toString();
+      }
+    }
+  }
+  for (var i = 0; i < xq.length; ++i) {
+    var a6 = parseFloat(Gf(Lc(nq, 8), 65535));
+    nq *= 65793;
+    nq &= parseFloat(4294967295);
+    nq += 4282663;
+    nq &= 8388607;
+    var yq = xq[i].toString();
+    for (var j = 1; j <= m6.length; j++) {
+      var st = (a6 % m6.length + j);
+      if (st % m6.length != arraySet('get',CharCodeAt(xq, i), j6)) continue;
+      var ls = st - a6 % m6.length;
+      yq = m6[ls >= m6.length ? ls - m6.length : ls].toString();
+    }
+
+    b6 += yq;
+  }
+  return b6;
+}
+function CharCodeAt(input, index)
+  {
+    return index >= input.length ? 0 : input[index];
+  }
+console.log(Wq(extract.substring(match[0].length), 3291446))
+
+
+function arraySet(method, index, array, value) {
+  if (method === 'get') {
+    return array.length > index ? array[index] : null;
+  }
+
+  if (method === 'set') {
+    if (array.length <= index) {
+      const totalToAdd = index - array.length;
+      for (let i = 0; i <= totalToAdd; i++) {
+        array.push(null);
+      }
+    }
+    array[index] = value;
+  }
+}
+
+
